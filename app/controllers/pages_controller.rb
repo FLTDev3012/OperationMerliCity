128cd0259ea5de1b706a8f776ec9488cdeb74bd6
dorian fiolet <dorian3012@hotmail.fr>
2024-01-03 07:36:24 +1000

    save

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :merlimontdavant ]
+  skip_before_action :authenticate_user!, only: [ :home, :merlimontdavant, :about ]
 
   def home
   end
 
   def merlimontdavant
+  end
+
+  def about
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   get "merlimontdavant", to: "pages#merlimontdavant"
+  get "about", to: "pages#about"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
