5577546677cd4075d97f85d4d252565442b327e5
dorian fiolet <dorian3012@hotmail.fr>
2024-01-12 23:41:17 +1000

    save

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,49 +8,49 @@
 Activite.destroy_all
 
 file = URI.open("https://a.cdn-hotels.com/gdcs/production186/d559/a82dee28-b6fd-417c-b51b-a535ddeb2f85.jpg?impolicy=fcrop&w=800&h=533&q=medium")
-activite1 = Activite.create(title: "surf", description: "au surfer merlimont plage")
+activite1 = Activite.create(title: "activite 1", description: "au surfer merlimont plage")
 activite1.photo.attach(io: file, filename: "v1.png", content_type: "image/png")
 activite1.save
 puts "#{Activite.count} activités have been created successfully!"
 
 file = URI.open("https://a.cdn-hotels.com/gdcs/production186/d559/a82dee28-b6fd-417c-b51b-a535ddeb2f85.jpg?impolicy=fcrop&w=800&h=533&q=medium")
-activite2 = Activite.create(title: "surf", description: "au surfer merlimont plage")
+activite2 = Activite.create(title: "activite 2", description: "au surfer merlimont plage")
 activite2.photo.attach(io: file, filename: "v2.png", content_type: "image/png")
 activite2.save
 puts "#{Activite.count} activités have been created successfully!"
 
 file = URI.open("https://a.cdn-hotels.com/gdcs/production186/d559/a82dee28-b6fd-417c-b51b-a535ddeb2f85.jpg?impolicy=fcrop&w=800&h=533&q=medium")
-activite3 = Activite.create(title: "surf", description: "au surfer merlimont plage")
+activite3 = Activite.create(title: "activite 3", description: "au surfer merlimont plage")
 activite3.photo.attach(io: file, filename: "v3.png", content_type: "image/png")
 activite3.save
 puts "#{Activite.count} activités have been created successfully!"
 
 file = URI.open("https://a.cdn-hotels.com/gdcs/production186/d559/a82dee28-b6fd-417c-b51b-a535ddeb2f85.jpg?impolicy=fcrop&w=800&h=533&q=medium")
-activite4 = Activite.create(title: "surf", description: "au surfer merlimont plage")
+activite4 = Activite.create(title: "activite 4", description: "au surfer merlimont plage")
 activite4.photo.attach(io: file, filename: "v4.png", content_type: "image/png")
 activite4.save
 puts "#{Activite.count} activités have been created successfully!"
 
 file = URI.open("https://a.cdn-hotels.com/gdcs/production186/d559/a82dee28-b6fd-417c-b51b-a535ddeb2f85.jpg?impolicy=fcrop&w=800&h=533&q=medium")
-activite5 = Activite.create(title: "surf", description: "au surfer merlimont plage")
+activite5 = Activite.create(title: "activite 5", description: "au surfer merlimont plage")
 activite5.photo.attach(io: file, filename: "v5.png", content_type: "image/png")
 activite5.save
 puts "#{Activite.count} activités have been created successfully!"
 
 file = URI.open("https://a.cdn-hotels.com/gdcs/production186/d559/a82dee28-b6fd-417c-b51b-a535ddeb2f85.jpg?impolicy=fcrop&w=800&h=533&q=medium")
-activite6 = Activite.create(title: "surf", description: "au surfer merlimont plage")
+activite6 = Activite.create(title: "activite 6", description: "au surfer merlimont plage")
 activite6.photo.attach(io: file, filename: "v6.png", content_type: "image/png")
 activite6.save
 puts "#{Activite.count} activités have been created successfully!"
 
 file = URI.open("https://a.cdn-hotels.com/gdcs/production186/d559/a82dee28-b6fd-417c-b51b-a535ddeb2f85.jpg?impolicy=fcrop&w=800&h=533&q=medium")
-activite7 = Activite.create(title: "surf", description: "au surfer merlimont plage")
+activite7 = Activite.create(title: "activite 7", description: "au surfer merlimont plage")
 activite7.photo.attach(io: file, filename: "v7.png", content_type: "image/png")
 activite7.save
 puts "#{Activite.count} activités have been created successfully!"
 
 file = URI.open("https://a.cdn-hotels.com/gdcs/production186/d559/a82dee28-b6fd-417c-b51b-a535ddeb2f85.jpg?impolicy=fcrop&w=800&h=533&q=medium")
-activite8 = Activite.create(title: "surf", description: "au surfer merlimont plage")
+activite8 = Activite.create(title: "activite 8", description: "au surfer merlimont plage")
 activite8.photo.attach(io: file, filename: "v8.png", content_type: "image/png")
 activite8.save
 puts "#{Activite.count} All activités have been created successfully!!!!"
