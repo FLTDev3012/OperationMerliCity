a22cfcc08701fd0d5fbe7fb1b7b6d31f55b9fc2f
dorian fiolet <dorian3012@hotmail.fr>
2024-01-20 18:58:13 +1000

    save

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,6 +9,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
+
   # POST /resource
   # def create
   #   super
@@ -59,4 +60,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  protected
+
+  # The path used after sign up.
+  def after_sign_up_path_for(resource)
+    faune_path
+  end
+
 end
