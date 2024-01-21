aa56f9a9006bbc93481d82feff67f0cfd7ca6ed7
dorian fiolet <dorian3012@hotmail.fr>
2024-01-21 19:02:08 +1000

    save

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+puts "Touts les Users ont etaient supprimer fu..."
 Activite.destroy_all
 puts "Toutes les activites ont etaient supprimer biloute..."
 Logement.destroy_all
