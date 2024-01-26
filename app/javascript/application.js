9a44900235a8414ff15a24ba217da9d05a30554f
dorian fiolet <dorian3012@hotmail.fr>
2024-01-26 14:56:54 +1000

    save

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,33 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+const signUp = (event) => {
+  event.preventDefault();
+  const email = document.getElementById("user_email").value;
+  const password = document.getElementById("user_password").value;
+  const password_confirmation = document.getElementById("user_password_confirmation").value;
+  const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+
+  const requestDetails = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRF-Token": csrfToken
+    },
+    body: JSON.stringify({
+      user: {
+        email: email,
+        password: password,
+        password_confirmation: password_confirmation
+      }
+    })
+  };
+
+  fetch("/users", requestDetails)
+    .then(response => response.json())
+    .then(data => console.log(data));
+};
+
+const form = document.querySelector("#form");
+form.addEventListener("submit", signUp);
