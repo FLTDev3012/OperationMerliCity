945cfc94309afdaf23d8b67fe77510f46f3417f9
dorian fiolet <dorian3012@hotmail.fr>
2024-02-08 20:01:45 +1000

    save

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :merlimontdavant, :about, :dashboard, :fauneflore, :faunecat, :univers ]
+  skip_before_action :authenticate_user!, only: [ :home, :merlimontdavant, :about, :dashboard, :fauneflore, :faunecat, :univers, :galerie ]
 
   def home
     @activites = Activite.first(5)
--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ module Testdevise
       generate.helper false
       generate.test_framework :test_unit, fixture: false
     end
+
+    # Configuration pour définir la langue par défaut à français
+    config.i18n.default_locale = :fr
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
