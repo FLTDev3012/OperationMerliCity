b12f1c65503b3367d504b698f9208bd1bf7cf26a
dorian fiolet <dorian3012@hotmail.fr>
2024-02-13 23:23:32 +1000

    save

--- db/seeds.rb
+++ db/seeds.rb
@@ -585,37 +585,37 @@ espece_poisson9.save
 puts "Activite en cours de creation ma couille... 🏄"
 
 file = URI.open("https://www.parcbagatelle.com/fileadmin/_processed_/6/8/csm_Banniere-BaggyPass-Bagatelle_fb50144a68.jpg")
-activite1 = Activite.create(title: "Bagatelle", description: "Bagatelle merli village")
+activite1 = Activite.create(title: "Bagatelle", description: "Parc d'attraction à merlimont village")
 activite1.photo.attach(io: file, filename: "v1.png", content_type: "image/png")
 activite1.save
 puts "#{Activite.count} activites crée!"
 
 file = URI.open("https://www.hillstonestlucia.com.au/wp-content/uploads/2020/09/0S1A5968.jpg")
-activite2 = Activite.create(title: "mini golf", description: "mini golf merlimont plage")
+activite2 = Activite.create(title: "Mini golf", description: "Mini golf de merlimont plage")
 activite2.photo.attach(io: file, filename: "v2.png", content_type: "image/png")
 activite2.save
 puts "#{Activite.count} activites crée!"
 
 file = URI.open("https://cdt14.media.tourinsoft.eu/upload/135-3571-IMG.jpg")
-activite3 = Activite.create(title: "char a voile", description: "char a voile merlimont plage")
+activite3 = Activite.create(title: "Char à voile", description: "Char à voile à merlimont plage")
 activite3.photo.attach(io: file, filename: "v3.png", content_type: "image/png")
 activite3.save
 puts "#{Activite.count} activites crée!"
 
 file = URI.open("https://a.cdn-hotels.com/gdcs/production186/d559/a82dee28-b6fd-417c-b51b-a535ddeb2f85.jpg?impolicy=fcrop&w=800&h=533&q=medium")
-activite4 = Activite.create(title: "activite 4", description: "au surfer merlimont plage")
+activite4 = Activite.create(title: "Activite 4", description: "Au surfer merlimont plage")
 activite4.photo.attach(io: file, filename: "v4.png", content_type: "image/png")
 activite4.save
 puts "#{Activite.count} activites crée!"
 
 file = URI.open("https://a.cdn-hotels.com/gdcs/production186/d559/a82dee28-b6fd-417c-b51b-a535ddeb2f85.jpg?impolicy=fcrop&w=800&h=533&q=medium")
-activite5 = Activite.create(title: "activite 5", description: "au surfer merlimont plage")
+activite5 = Activite.create(title: "Activite 5", description: "Au surfer merlimont plage")
 activite5.photo.attach(io: file, filename: "v5.png", content_type: "image/png")
 activite5.save
 puts "#{Activite.count} activites crée!"
 
 file = URI.open("https://a.cdn-hotels.com/gdcs/production186/d559/a82dee28-b6fd-417c-b51b-a535ddeb2f85.jpg?impolicy=fcrop&w=800&h=533&q=medium")
-activite6 = Activite.create(title: "activite 6", description: "au surfer merlimont plage")
+activite6 = Activite.create(title: "Activite 6", description: "Au surfer merlimont plage")
 activite6.photo.attach(io: file, filename: "v6.png", content_type: "image/png")
 activite6.save
 puts "#{Activite.count} activites crée!"
