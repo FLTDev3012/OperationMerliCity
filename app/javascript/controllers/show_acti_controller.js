3c772b81ec4a6130bab447df154221bf37221436
dorian fiolet <dorian3012@hotmail.fr>
2024-03-16 18:48:43 +1000

    save stimulus done pb posution cto container

--- app/javascript/controllers/show_acti_controller.js
+++ app/javascript/controllers/show_acti_controller.js
@@ -7,4 +7,29 @@ export default class extends Controller {
     console.log("Hello World! from acti show");
   }
 
+  openaddress() {
+    // Toggle la classe d-none pour afficher ou masquer la section
+    this.addressTarget.classList.remove("d-none");
+    this.priceTarget.classList.add("d-none");
+    this.hoursTarget.classList.add("d-none");
+    console.log("open adresse");
+  }
+
+  openprice() {
+    // Toggle la classe d-none pour afficher ou masquer la section
+    this.addressTarget.classList.add("d-none");
+    this.priceTarget.classList.remove("d-none");
+    this.hoursTarget.classList.add("d-none");
+    console.log("open price");
+  }
+
+  openhours() {
+    // Toggle la classe d-none pour afficher ou masquer la section
+    this.addressTarget.classList.add("d-none");
+    this.priceTarget.classList.add("d-none");
+    this.hoursTarget.classList.remove("d-none");
+    console.log("open hours");
+  }
+
+
 }
