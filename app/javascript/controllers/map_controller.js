d83cc350b6d98c7e02d73cfe571c865d5cd338c3
dorian fiolet <dorian3012@hotmail.fr>
2024-03-26 16:47:24 +1000

    save

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -18,7 +18,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/fltev3012/clu7znchq005n01r65h9n16qe"
     })
 
   }
