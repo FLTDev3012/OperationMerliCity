22b8e6698b177247545c21279a1ee0b2ea65a348
dorian fiolet <dorian3012@hotmail.fr>
2024-04-15 19:39:10 +1000

    test de mettre un script pour enlever le turbo rails a tout les liens

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,9 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+document.addEventListener("turbo:load", function() {
+  document.querySelectorAll("a").forEach(function(link) {
+    link.setAttribute("data-turbo", "false");
+  });
+});
